520ef932ebf51f852acfce91a90df995d7ac2fa5
Viet Shell <vietstack@gmail.com>
2014-05-07 08:35:09 +0700

    fix sed error

--- setup_interface.sh
+++ setup_interface.sh
@@ -10,7 +10,7 @@ ip link show | grep BROADCAST,MULTICAST | awk '{print $2}' | sed 's/://' > list_
 for interface in $( cat list_allinterface.txt );
 do ipadd=`/sbin/ifconfig $interface | grep inet | awk '{print $2}' | sed 's/addr://'`
 macaddr=`ip link show $interface | grep link/ether | awk '{print $2}'`
-netmask=`ifconfig $interface | grep Mask | awk '{print $4}' | sedq 's/Mask://'`
+netmask=`ifconfig $interface | grep Mask | awk '{print $4}' | sed 's/Mask://'`
 kieu=`cat /etc/sysconfig/network-scripts/ifcfg-$interface | grep BOOTPROTO | sed 's/BOOTPROTO=//'`
 if [ "$kieu" = "none" ]; then
 kieu=static
